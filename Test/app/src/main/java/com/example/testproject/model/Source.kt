2f2075dfb917082ef399601ce7b72ac7f2eed086
AungKhantOo99 <67188965+AungKhantOo99@users.noreply.github.com>
2023-02-23 10:50:07 +0630

    Delete Test directory

--- Test/app/src/main/java/com/example/testproject/model/Source.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testproject.model
-
-import java.io.Serializable
-
-data class Source(
-    val id: String,
-    val name: String
-): Serializable
--- Test/app/src/main/java/com/example/testproject/model/Tag.kt
+++
@@ -1,7 +0,0 @@
-package com.example.testproject.model
-
-import java.io.Serializable
-
-class Tag (val warranty:Boolean,
-           val TripleCamera:Boolean,
-           val InScreenFinger:Boolean): Serializable
